0a7927ce2617a6ae33d8fcb113f73ac295dc0ec3
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-17 11:19:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ let level2=0
 function insert_Row() {
 	level+=2
 	level2+=2
-	let child_td = document.createElement('tr')
-	child_td.innerHTML=`
-	<td>New Cell${level}</td> 
-	<td>New Cell${level2}</td>
-	`
-	table.insertBefore(child_td,table.firstChild)
+	let child_tr = document.createElement('tr')
+	let child_td1 = document.createElement('td')
+	let child_td2 = document.createElement('td')
+	
+	child_td2.textContent = `New Cell${level}`
+	child_tr.appendChild(child_td2)
+	child_td1.textContent = `New Cell${level2}`
+	child_tr.appendChild(child_td1)
+	
+	table.insertBefore(child_tr,table.firstChild)
 
 }
