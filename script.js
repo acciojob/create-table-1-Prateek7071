7c1d6470bdd4d1837be6eca95f6ff930a968adf0
Prateek <106758759+Prateek7071@users.noreply.github.com>
2026-01-17 10:02:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const table=  document.getElementById('sampleTable')
-let level=2
+let level=0
+let level2=1
 function insert_Row() {
-	level++
+	level+=2
+	level2+=2
 	let child_td = document.createElement('tr')
 	child_td.innerHTML=`
-	<td>New cell1</td> 
-	<td>New cell2</td>
+	<td>New cell${level}</td> 
+	<td>New cell${level2}</td>
 	`
 	table.appendChild(child_td)
 }
